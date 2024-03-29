98556b18d7cebbe27dcc2858ba6ee790b60fd582
Aziz Karagülmez <123738266+azizkaragulmez@users.noreply.github.com>
2024-03-30 00:05:53 +0300

    Faktöriyel Bulma

---
+++ java101 easy apps/src/O_FaktöriyelH.java
@@ -0,0 +1,17 @@
+public class O_FaktöriyelH {
+
+    public static void main (String[] args)
+    {
+        int number=1,fak=5,top=0;
+
+        for ( ;number<=fak;number++ )
+        {
+            top+=number;
+            System.out.println(number);
+        }
+        System.out.println(top);
+
+
+    }
+
+}
