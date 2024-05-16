843b93cd56c9e286f0f258a979c7bb48593245b8
Aziz Karagülmez <123738266+azizkaragulmez@users.noreply.github.com>
2024-05-16 23:13:58 +0300

    Manth and String metot

---
+++ java101 easy apps_Diğer_K_Math_String_Sınıfı/Math_Sinifi/src/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println((Math.random()*100));  //0-100 arası random sayı üretir
+
+        System.out.println(Math.sqrt(3));   //karekökü alma
+
+        System.out.println(Math.pow(2,3));
+
+
+    }
+
+}
